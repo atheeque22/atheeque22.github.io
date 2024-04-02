8708f71f18098a2eb6e0c60142970e52732e5ea3
atheeque22 <155919120+atheeque22@users.noreply.github.com>
2024-04-02 17:33:25 +0530

    Add files via upload

---
+++ web_cw/res/js/main.js
@@ -0,0 +1,12 @@
+let sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+};
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+};
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
+
